c195bdd89e5301032a82ddb7b92139232cfcbe14
LukasHeiniger <111046378+LukasHeiniger@users.noreply.github.com>
2024-12-15 21:13:34 +0100

    commit 1

---
+++ cypress/integration/theme_toggle_spec.js
@@ -0,0 +1,24 @@
+describe('Theme Toggle Button Test', () => {
+  it('sollte das Theme wechseln, wenn der Button geklickt wird', () => {
+
+    cy.visit('https://lukasheiniger.github.io/LBM324/');
+
+
+    cy.get('#theme-toggle').should('exist');
+
+ 
+    cy.get('html').should('not.have.attr', 'data-theme', 'dark');
+
+
+    cy.get('#theme-toggle').click();
+
+
+    cy.get('html').should('have.attr', 'data-theme', 'dark');
+
+
+    cy.get('#theme-toggle').click();
+
+ 
+    cy.get('html').should('not.have.attr', 'data-theme', 'dark');
+  });
+});
